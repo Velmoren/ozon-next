87e7624b0fea6e920fdd1e4a859ed21fd8ccab24
Velmoren <velmoren@yahoo.com>
2025-10-22 11:57:43 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="container">
+      <div className="row">
+        <div className="col-3 col-xl-2 d-none d-lg-block">
+          <div className="filter">
+            <div className="filter-title">
+              <h5>Фильтр</h5>
+            </div>
+            <div className="filter-price">
+              <div className="filter-price_title">
+                Цена
+              </div>
+              <form>
+                <div className="filter-price_range">
+                  <div className="filter-price_input-wrapper">
+                    <label htmlFor="min" className="filter-price_label">от</label>
+                    <input id="min" className="filter-price_input" />
+                  </div>
+                  <div className="filter-price_input-wrapper">
+                    <label htmlFor="max" className="filter-price_label">до</label>
+                    <input id="max" className="filter-price_input" />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="filter-check">
+              <label className="filter-check_label">
+                <input type="checkbox" className="filter-check_checkbox" id="discount-checkbox" />
+                <span className="filter-check_checkmark"></span>
+                <span className="filter-check_label-text">Акция</span>
+              </label>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="col-12 col-lg-9 col-xl-10">
+          <div className="container">
+            <div className="row no-gutters goods">
+              {/* <!-- КАРТОЧКИ ТОВАРОВ -->
+                  <!-- END КАРТОЧКИ ТОВАРОВ --> */}
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }

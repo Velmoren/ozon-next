b55ec6f580621851db9cb7038180b60bcbe6bdeb
Velmoren <velmoren@yahoo.com>
2025-11-14 18:45:35 +0300

    fix name cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './scss/style.scss'
 import localFont from 'next/font/local'
 import Header from './ui/header';
-import Cart from './ui/cart';
+import Cart from './ui/сart';
 
 const GTEestiProText = localFont({
   src: [
